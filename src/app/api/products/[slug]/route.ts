dc2149c41dd72e0f92b6df1cb80313a4203b0d76
Jordane Chaves <jordane_chaves@yahoo.com.br>
2025-07-23 19:17:36 -0300

    feat: add product page

---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,22 @@
+import z from 'zod'
+
+import data from '../data.json'
+
+export async function GET(
+  _request: Request,
+  { params }: { params: Promise<{ slug: string }> },
+) {
+  const parmasData = await params
+
+  const slug = z.string().parse(parmasData.slug)
+
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const product = data.products.find((product) => product.slug === slug)
+
+  if (!product) {
+    return Response.json({ message: 'Product not found.' }, { status: 400 })
+  }
+
+  return Response.json(product)
+}
