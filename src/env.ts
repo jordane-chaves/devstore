f6484583cac9d1f48a5b76e34c8f26103285bc9a
Jordane Chaves <jordane_chaves@yahoo.com.br>
2025-08-01 20:14:29 -0300

    chore: add client/server env variables boundary

--- src/env.ts
+++ src/env.ts
@@ -1,19 +1,17 @@
+import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.url(),
-  APP_URL: z.url(),
+export const env = createEnv({
+  server: {
+    APP_URL: z.url(),
+  },
+
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.url(),
+  },
+
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 })
-
-const envParsed = envSchema.safeParse(process.env)
-
-if (!envParsed.success) {
-  console.error(
-    'Invalid environment variables',
-    z.flattenError(envParsed.error).fieldErrors,
-  )
-
-  throw new Error('Invalid environment variables.')
-}
-
-export const env = envParsed.data
